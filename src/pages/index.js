27d2225123837ccfdb63c8687ad95463e34a04ce
Ben Stephan <ben.stephan10@gmail.com>
2020-07-24 10:07:39 -0500

    with video

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,19 @@ const BlogIndex = ({ data }) => {
       <video
         className="video-player"
         // width="100%"
-        height="100%"
+        // height="100%"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          height: "100%",
+          width: "100%",
+          zIndex: "-1",
+          objectFit: "fill",
+          objectPosition: "center",
+        }}
         loop
         muted
         autoPlay
